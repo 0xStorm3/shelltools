d8e2cfe1975170456289649de89a0697dd64d347
0xStorm3 <waldenj6@proton.me>
2023-03-09 12:21:16 +0800

    blockchaininfo

--- notification.sh
+++ notification.sh
@@ -1,16 +1,28 @@
 #!/bin/ash
 
+echo "BEGIN"
+
 bci=$(bitcoin-cli -datadir=/mnt/sda/bitcoin getblockchaininfo)
 echo $bci
 
 blocks=$(echo $bci | jq .blocks)
 headers=$(echo $bci | jq .headers)
 idb=$(echo $bci | jq .initialblockdownload)
+bitcoindstatus=""
 
 if idb="true"
 then
-    echo $(printf "%d%%" $(($blocks*100/$headers)))
-    echo 'IBD '
+    percent=$(printf "%d%%" $(($blocks*100/$headers)))
+    bitcoindstatus='IBD ${percent}'
 else
-    echo "RUNNING"
+    bitcoindstatus="RUNNING ${headers}"
 fi
+
+echo $bitcoindstatus
+if -n $MGR_USER
+then 
+    echo "send message to $MGR_USER"
+    noscl message $MGR_USER "$bitcoindstatus"
+fi
+
+echo "END"
